4199c2edde745008ec66e1aef4f471446d3ff9ef
Thomas Nguyen <developer.fps.gg@gmail.com>
2019-10-10 23:30:08 -0400

    first phase

--- main.js
+++ main.js
@@ -62,3 +62,5 @@ jsChain.addBlock(new Block("12/26/2017", { amount: 10 }));
 
 console.log(JSON.stringify(jsChain, null, 4));
 console.log("Is blockchain valid? " + jsChain.checkValid());
+
+// Phase one complete
